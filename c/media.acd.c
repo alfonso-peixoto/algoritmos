db2ca0ed3516dcc2ed90f16b7663f0f05a7ec20a
André Alfonso Peixoto <alfonso.peixoto@gmail.com>
2020-10-15 20:06:05 -0500

    Create media.acd.c

---
+++ c/media.acd.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+main()
+{
+	int num;
+	printf("Informe um numero:");
+	scanf("%d",&num);
+	if (num%2 == 0)
+	{
+		printf("Par\n");
+	}
+	else
+	{
+		printf("Impar\n");
+	}
+	
+	system("PAUSE");
+}
