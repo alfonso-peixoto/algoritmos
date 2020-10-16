0458c1e4fb8a80c416c463c4a9447398ff67812c
André Alfonso Peixoto <alfonso.peixoto@gmail.com>
2020-10-15 20:07:28 -0500

    Create media.acd.c

---
+++ c/media.acd.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+// Código para calcular as notas e média de um aluno da U:VERSE
+// Média mínima para aprovação: 7,0
+// Média mínima para prova final: 5,0
+
+main()
+
+{
+
+	float n1, n2, media;
+	
+	printf("Digite a primeira nota: ");
+	scanf("%f",&n1);
+	
+	printf("Digite a segunda nota: ");
+	scanf("%f",&n2);
+	
+	media = (n1+n2) / 2;
+	printf("Media: %f\n", media);
+	
+	if (media > 7)
+	{
+		printf("Aluno aprovado");
+	}
+	else if (media > 5)
+	{
+		printf("Aluno em prova final");
+	}
+	else
+	{
+		printf("Aluno reprovado");
+	}
+}
