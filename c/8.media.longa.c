3d97bc179a41ed47c92831b007b97183c8983039
André Alfonso Peixoto <alfonso.peixoto@gmail.com>
2020-10-29 20:15:21 -0500

    Update 8.media.longa.c

--- c/8.media.longa.c
+++ c/8.media.longa.c
@@ -1,5 +1,5 @@
 /*
-8 - Fazer um programa no qual o usu·rio vai entrando sucessivamente com valores positivos. Quando o usu·rio entrar com um valor negativo o programa para de pedir valores e calcula a mÈdia dos valores j· fornecidos.
+8 - Fazer um programa no qual o usu√°rio vai entrando sucessivamente com valores positivos. Quando o usu√°rio entrar com um valor negativo o programa para de pedir valores e calcula a m√©dia dos valores j√° fornecidos.
 */
 
 #include<stdio.h>
@@ -7,37 +7,25 @@
 
 main()
 {
-	float n1, n2;
-	float soma, subtracao, multiplicacao, divisao;
-	char operacao;
-	printf("Informe o primeiro numero: ");
-	scanf("%f",&n1);
-	printf("Informe o segundo numero: ");
-	scanf("%f",&n2);
-	printf("Informe a operacao: ");
-	scanf("%s",&operacao);
-	if (operacao == '+')
+	float numero;
+	float media = 0;
+	int i = 1;
+	int controlador = 0;
+	while (controlador == 0)
 	{
-		soma = n1 + n2;
-		printf("Resultado da soma: %f", soma);
+		printf("Informe o numero %d: ", i);
+		scanf("%f", &numero);
+		if (numero >= 0)
+		{
+			media += numero;
+			i++;
+		}
+		else
+		{
+			controlador = 1;
+		}
 	}
-	else if (operacao == '-')
-	{
-		subtracao = n1 - n2;
-		printf("Resultado da subtracao: %f", subtracao);
-	}
-	else if (operacao == '*')
-	{
-		multiplicacao = n1 * n2;
-		printf("Resultado da multiplicacao: %f", multiplicacao);
-	}
-	else if (operacao == '/')
-	{
-		divisao = n1 / n2;
-		printf("Resultado da divis„o: %f", divisao);
-	}
-	else
-	{
-		printf("Operacao invalida (+, -, * ou /)");
-	}
+	media /= i;
+	printf("A media e %f: ", media);
+	return 0;
 }
