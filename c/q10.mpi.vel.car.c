2e45b96e3669d78777e850997a451cbb6bb73dc0
André Alfonso Peixoto <alfonso.peixoto@gmail.com>
2020-12-06 13:46:06 -0500

    Update q10.mpi.vel.car.c

--- c/q10.mpi.vel.car.c
+++ c/q10.mpi.vel.car.c
@@ -1,38 +1,27 @@
-#include <stdio.h>
-#include <string.h>
-#include <math.h>
-#include <stdlib.h>
-#include "mpi.h"
 /*
-Faça um programa em C como MPI para calcular a velocidade de um carro em 3 processos, o processo
-1 envia o tempo para o 0, o processo 2 envia a distancia para o 0, o 0 calcula velocidade de acordo
-com a formula: Velocidade = Distancia/Tempo. No final o processo 0 mostra o resultado.
+Faça um programa que receba uma frase (máximo 100 caracteres) e uma letra qualquer, calcule e mostre a quantidade que essa letra aparece na frase digitada. 
 */
 
-int main(int argc, char** argv)
+#include<stdio.h>
+#include<stdlib.h>
+
+main()
 {
-	int meu_rank, np, origem, destino, tag=0;
-	float d,t,v;
-	MPI_Status status;
-	MPI_Init(&argc, &argv);
-	MPI_Comm_rank(MPI_COMM_WORLD, &meu_rank);
-	MPI_Comm_size(MPI_COMM_WORLD,&np);
-	if(meu_rank == 1){
-        t = 1;
-        MPI_Send(&t, 1, MPI_REAL, 0, tag, MPI_COMM_WORLD);
-    }
-    
-    else if(meu_rank == 2){
-        d = 1;
-        MPI_Send(&d, 1, MPI_REAL, 0, tag, MPI_COMM_WORLD);
-    }  
-    
-	else if (meu_rank == 0) {
-        MPI_Recv(&t, 1, MPI_REAL, 1, tag, MPI_COMM_WORLD, &status);
-        MPI_Recv(&d, 1, MPI_REAL, 2, tag, MPI_COMM_WORLD, &status);
-		v = d/t;
-		printf("A velocidade e %f\n", v);
+	int i;
+	char str[100];
+	char c;
+	int aparicoesLetra = 0;
+	printf("Informe uma String: ");
+    fgets(str, 100, stdin);
+    printf("Informe um caractere: ");
+    scanf(" %c", &c);
+	for (i=0; i<100; i++)
+	{
+		if (str[i] == c)
+		{
+			aparicoesLetra += 1;
+		}
 	}
-	
-    MPI_Finalize( );
+	printf("A letra aparece %d vezes na frase digitada", aparicoesLetra);
+    return 0;
 }
